f24cd31333b46f53f03d89a00011af5fd059f232
derrick chambers <djachambeador@gmail.com>
2023-06-19 10:53:55 -0600

    update with patch merge check

--- test_spool_processing.py
+++ test_spool_processing.py
@@ -2,6 +2,8 @@
 Simple test to try to reproduce dascore issue #171.
 """
 
+from pathlib import Path
+
 import pytest
 
 import dascore as dc
@@ -12,6 +14,9 @@ from proc import std
 @pytest.fixture()
 def real_terra15_data_spool():
     """Get a small spool for testing."""
+    path = Path("test_data")
+    if not path.exists():
+        pytest.skip(f"{path} not found; add 'real' data to it run test.")
     return dc.spool("test_data").update()
 
 
@@ -21,7 +26,7 @@ def synth_data_memory_spool():
     spool = dc.get_example_spool(
         "random_das",
         starttime="2023-06-13T15:38:00.49953408",
-        d_time=1/100,
+        d_time=1 / 100,
         shape=(100, 100),
         length=60,
     )
@@ -40,6 +45,9 @@ def test_proc_real_data(real_terra15_data_spool, tmp_path_factory):
     """Ensure the proc works on real terra15 data."""
     out = tmp_path_factory.mktemp("std_out_real_data")
     std(real_terra15_data_spool, str(out))
+    new = dc.spool(out).update()
+    # merge is successful if we have only one patch
+    assert len(new) == 1
 
 
 def test_proc_synth_data_memory(synth_data_memory_spool, tmp_path_factory):
@@ -47,6 +55,9 @@ def test_proc_synth_data_memory(synth_data_memory_spool, tmp_path_factory):
     spool = synth_data_memory_spool
     out = tmp_path_factory.mktemp("std_synth_data_memory")
     std(spool, str(out))
+    new = dc.spool(out).update()
+    # merge is successful if we have only one patch
+    assert len(new) == 1
 
 
 def test_proc_synth_data_directory(synth_data_directory_spool, tmp_path_factory):
@@ -54,3 +65,6 @@ def test_proc_synth_data_directory(synth_data_directory_spool, tmp_path_factory)
     spool = synth_data_directory_spool
     out = tmp_path_factory.mktemp("std_synth_data_memory")
     std(spool, str(out))
+    new = dc.spool(out).update()
+    # merge is successful if we have only one patch
+    assert len(new) == 1
